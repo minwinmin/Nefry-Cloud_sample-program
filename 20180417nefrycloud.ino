1c8363337f3e0f024dc80a752c4215b4e52e506c
minwinmin <takuro.mikami@gmail.com>
2018-04-18 11:54:01 +0900

    Add files via upload

---
+++ 20180417nefrycloud.ino
@@ -0,0 +1,23 @@
+#include <Nefry.h>
+#include <NefryCloud.h>
+
+NefryCloud nefryCloud;
+
+void onpush(String message);
+
+void setup() {
+  nefryCloud.begin("takudooon","77c58ea49b3d798f727e5af156fdc9fcb36f4f63cc0e801c2310e07d4fe59344");//サイトで登録したuser,メールで受け取ったapikeyを入力してください
+  nefryCloud.on(onpush);
+}
+
+void loop() {
+  nefryCloud.loop();
+}
+
+void onpush(String message) {//Nefryクラウド(仮)から通知が来ます
+  Nefry.print("onpush : ");
+  Nefry.println(message);
+  if(message.equals("Nefry")){
+    Nefry.println("Hello");
+  }
+}
